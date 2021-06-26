8c7c0a0a32660161c986fe2271a7d73bafb825ce
henimex <49072861+henimex@users.noreply.github.com>
2021-06-27 00:42:20 +0300

    Variables

--- 74/PredefinedJS.js
+++ 74/PredefinedJS.js
@@ -86,7 +86,7 @@
 		var strValue6 = new Date();
 				
 
-		document.write("<br/>" + "String" + "<br/>");
+		document.write("<br/>" + "String [Original Vers]" + "<br/>");
 		document.write("----------------------" + "<br/>");
 		document.write("Value 1 / Type : " + strValue1 + " " + typeof strValue1 + "<br/>"); 
 		document.write("Value 2 / Type : " + strValue2 + " " + typeof strValue2 + "<br/>"); 
@@ -102,10 +102,61 @@
 		var convertedStrValue5 = String(strValue5);
 		var convertedStrValue6 = String(strValue6);
 		
-		document.write("----------------------" + "<br/>");
+		document.write("----------------------[Converted To String]" + "<br/> <br/>");
 		document.write("Value 1 / Type : " + convertedStrValue1 + " " + typeof convertedStrValue1 + "<br/>"); 
 		document.write("Value 2 / Type : " + convertedStrValue2 + " " + typeof convertedStrValue2 + "<br/>"); 
 		document.write("Value 3 / Type : " + convertedStrValue3 + " " + typeof convertedStrValue3 + "<br/>"); 
 		document.write("Value 4 / Type : " + convertedStrValue4 + " " + typeof convertedStrValue4 + "<br/>"); 
 		document.write("Value 5 / Type : " + convertedStrValue5 + " " + typeof convertedStrValue5 + "<br/>"); 
 		document.write("Value 6 / Type : " + convertedStrValue6 + " " + typeof convertedStrValue6 + "<br/>"); 
+
+		//Number
+
+		var convertedIntValue1 = Number(strValue1);
+		var convertedIntValue2 = Number(strValue2);
+		var convertedIntValue3 = Number(strValue3);
+		var convertedIntValue4 = Number(strValue4);
+		var convertedIntValue5 = Number(strValue5);
+		var convertedIntValue6 = Number(strValue6);
+		
+		document.write("----------------------[Converted To Int]" + "<br/> <br/>");
+		document.write("Value 1 / Type : " + convertedIntValue1 + " " + typeof convertedIntValue1 + "<br/>"); 
+		document.write("Value 2 / Type : " + convertedIntValue2 + " " + typeof convertedIntValue2 + "<br/>"); 
+		document.write("Value 3 / Type : " + convertedIntValue3 + " " + typeof convertedIntValue3 + "<br/>"); 
+		document.write("Value 4 / Type : " + convertedIntValue4 + " " + typeof convertedIntValue4 + "<br/>"); 
+		document.write("Value 5 / Type : " + convertedIntValue5 + " " + typeof convertedIntValue5 + "<br/>"); 
+		document.write("Value 6 / Type : " + convertedIntValue6 + " " + typeof convertedIntValue6 + "<br/> <br/> <br/>"); 
+
+		//10-Value Of
+
+		var val10_1 = 5;
+		var val10_2 = "5";
+		var val10_3 = val10_1.valueOf();
+		var val10_4 = val10_2.valueOf();
+
+
+		document.write("Value Of" + "<br/> <br/>");
+		document.write("----------------------" + "<br/> <br/>");
+		document.write("Var 1 / Type : " + val10_1 + " " + typeof val10_1 + "<br/>"); 
+		document.write("Var 2 / Type : " + val10_2 + " " + typeof val10_2 + "<br/>"); 
+		document.write("Var 3 / Type : " + val10_3 + " " + typeof val10_3 + "<br/>"); 
+		document.write("Var 4 / Type : " + val10_4 + " " + typeof val10_4 + "<br/> <br/>"); 
+		
+
+		//11-Index Of
+
+		var val11_1 = "Joseph HenimeX github adress is : https://github.com/henimex";
+		var val11_2 = val11_1.indexOf("me")
+		var val11_3 = val11_1.indexOf("me",15);
+		var val11_4 = val11_1.lastIndexOf("henimex");
+		var val11_5 = val11_1.lastIndexOf("is",-10);
+
+		document.write("Index Of && LastIndex Of" + "<br/> <br/>");
+		document.write("----------------------" + "<br/> <br/>");
+		document.write("Var 1 : " + val11_1 + "<br/>"); 
+		document.write("Var 2 : " + val11_2 + "<br/>"); 
+		document.write("Var 3 : " + val11_3 + "<br/>"); 
+		document.write("Var 3 : " + val11_4 + "<br/>"); 
+		document.write("Var 3 : " + val11_5 + "<br/>"); 
+
+
